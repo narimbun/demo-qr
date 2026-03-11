eb852892641a8f94f49240146bb6b7cf3423580b
Ronal Narimbun <RONALN@intra.excelcom.co.id>
2026-03-11 08:33:48 +0700

    pilit

---
+++ src/test/java/com/example/demo_qr/DemoQrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_qr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoQrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
